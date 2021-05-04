16af0add1458960507e69ab93144731db7272d87
Henrik <crafter.birne@gmx.de>
2021-05-05 00:54:07 +0200

    Final Version really

--- src/main/java/h02/list_of_arrays/ListOfArrays.java
+++ src/main/java/h02/list_of_arrays/ListOfArrays.java
@@ -43,12 +43,12 @@ public class ListOfArrays<T> implements List<T> {
 
   @Override
   public int size() {
-    return 0;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public boolean isEmpty() {
-    return false;
+    throw new UnsupportedOperationException();
   }
 
   @Override
@@ -88,44 +88,6 @@ public class ListOfArrays<T> implements List<T> {
   public <T1> T1[] toArray(T1[] a) {
     throw new UnsupportedOperationException();
   }
-
-
-  /*
-  public boolean add_old(T t) {
-    //Erste freie Stelle finden
-
-    if (tail == null) {
-      tail = head = new ListOfArraysItem<>(LENGTH_OF_ALL_ARRAYS);
-      tail.arrayOfElems[0] = t;
-      tail.numberOfListElemsInArray++;
-      return true;
-    }
-    var array = tail.arrayOfElems;
-    for (int i = 0; i < array.length; i++) {
-      //wenn freie stelle im array
-      if (array[i] == null) {
-        //objekt einfügen, counter hochzählen
-        array[i] = t;
-        tail.numberOfListElemsInArray++;
-        return true;
-      }
-    }
-    //Array spalten und element einfügen
-    var oldListOfArraysTail = tail;
-    tail = tail.next = new ListOfArraysItem<>(LENGTH_OF_ALL_ARRAYS);
-    tail.previous = oldListOfArraysTail;
-    var newArray = tail.arrayOfElems;
-    //Array halbieren
-    for (int i = 0, j = LENGTH_OF_ALL_ARRAYS / 2; i < LENGTH_OF_ALL_ARRAYS / 2; i++, j++) {
-      newArray[j] = array[j];
-      array[j] = null;
-    }
-    //neues element einfügen
-    newArray[LENGTH_OF_ALL_ARRAYS / 2] = t;
-    tail.numberOfListElemsInArray++;
-    return true;
-  }
-*/
 
   @Override
   public boolean add(T t) {
@@ -168,12 +130,12 @@ public class ListOfArrays<T> implements List<T> {
 
   @Override
   public boolean remove(Object o) {
-    return false;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public boolean containsAll(Collection<?> c) {
-    return false;
+    throw new UnsupportedOperationException();
   }
 
   @Override
@@ -207,90 +169,24 @@ public class ListOfArrays<T> implements List<T> {
     return true;
   }
 
-
-/*
-  public boolean addAll_half_filling(Collection<? extends T> c) {
-    if (c != null && head == null) {
-      head = tail = new ListOfArraysItem<>(LENGTH_OF_ALL_ARRAYS);
-    } else if (c == null)
-      throw new NullPointerException();
-    //Menge der nötigen neuen arrays finden und alle erzeugen
-
-
-    //wenn alle elemente aus c nicht mehr ins aktuelle array passen und das jetzige schon mehr als halb voll ist, dann aufspalten
-    int countNewArrays = 0;
-    if (c.size() + tail.numberOfListElemsInArray > LENGTH_OF_ALL_ARRAYS) {
-      //anzahl der neuen zusätzlichen arrays berechnen
-      boolean firstArray = true;
-      int newElements = c.size();
-      while (newElements > 0) {
-        if (firstArray) {
-          if (tail.numberOfListElemsInArray + newElements > LENGTH_OF_ALL_ARRAYS) {
-            var oldListOfArraysTail = tail;
-            tail = tail.next = new ListOfArraysItem<>(LENGTH_OF_ALL_ARRAYS);
-            tail.previous = oldListOfArraysTail;
-            for (int i = 0, j = LENGTH_OF_ALL_ARRAYS / 2; i < LENGTH_OF_ALL_ARRAYS / 2; i++, j++) {
-              tail.arrayOfElems[j] = oldListOfArraysTail.arrayOfElems[j];
-              oldListOfArraysTail.arrayOfElems[j] = null;
-            }
-          }
-          newElements -= tail.numberOfListElemsInArray;
-          firstArray = false;
-        } else {
-          if (newElements - LENGTH_OF_ALL_ARRAYS > 0) {
-            countNewArrays++;
-            newElements = -LENGTH_OF_ALL_ARRAYS / 2;
-          } else {
-            newElements -= LENGTH_OF_ALL_ARRAYS;
-          }
-        }
-      }
-    }
-    var items = c.stream().iterator();
-    //Elemente hinzufügen
-    while (countNewArrays > 0) {
-      //halbes array füllen
-      for (int i = 0; i < LENGTH_OF_ALL_ARRAYS / 2 && items.hasNext(); i++) {
-        tail.arrayOfElems[i] = items.next();
-        tail.numberOfListElemsInArray++;
-      }
-      var temp = tail;
-      tail = tail.next = new ListOfArraysItem<>(LENGTH_OF_ALL_ARRAYS);
-      tail.previous = temp;
-      countNewArrays--;
-    }
-
-    //letztes array mit restlichen elementen füllen
-
-    for (int i = 0; i < LENGTH_OF_ALL_ARRAYS && items.hasNext(); i++) {
-      tail.arrayOfElems[i] = items.next();
-      tail.numberOfListElemsInArray++;
-    }
-
-
-    return true;
-  }
-*/
-
-
   @Override
   public boolean addAll(int index, Collection<? extends T> c) {
-    return false;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public boolean removeAll(Collection<?> c) {
-    return false;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public boolean retainAll(Collection<?> c) {
-    return false;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void clear() {
-
+    throw new UnsupportedOperationException();
   }
 
   @Override
@@ -314,42 +210,42 @@ public class ListOfArrays<T> implements List<T> {
 
   @Override
   public T set(int index, T element) {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void add(int index, T element) {
-
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public T remove(int index) {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public int indexOf(Object o) {
-    return 0;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public int lastIndexOf(Object o) {
-    return 0;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public ListIterator<T> listIterator() {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public ListIterator<T> listIterator(int index) {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public List<T> subList(int fromIndex, int toIndex) {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
 
